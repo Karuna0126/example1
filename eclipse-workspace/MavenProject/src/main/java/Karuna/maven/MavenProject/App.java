455ab3fd1d0654794a6be555a899fec19d4df078
Karuna Kalasannavar <karuna.kalasannavar@wipro.com>
2022-09-03 21:06:43 +0530

    1st commit

---
+++ eclipse-workspace/MavenProject/src/main/java/Karuna/maven/MavenProject/App.java
@@ -0,0 +1,9 @@
+package Karuna.maven.MavenProject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven github jenkins" );
+    }
+}
